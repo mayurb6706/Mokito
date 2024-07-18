3912e806f38ec24344500464363ca48fb541bdb9
Bhosale <mayur.b.bhosale@capgemini.com>
2024-07-18 17:00:37 +0530

    Mock with List

--- src/test/java/com/cwm/mokito/serviceImpl/ListMock.java
+++ src/test/java/com/cwm/mokito/serviceImpl/ListMock.java
@@ -13,47 +13,53 @@ public class ListMock {
 
 	@Test
 	public void testMokingListInterface() {
-	//Mocking list interface
-		
-		List list= mock(List.class);
-		
-		when(list.size()).thenReturn(3);
-		
-		assertEquals(3, list.size());
-		 }
+		// Mocking list interface
 
-	
+		List list = mock(List.class);
+
+		when(list.size()).thenReturn(3);
+
+		assertEquals(3, list.size());
+	}
+
 	@Test
 	public void testMockingInterfaceWithMultipleValue() {
 		List list = mock(List.class);
-		
+
 		when(list.size()).thenReturn(2).thenReturn(4);
-		
+
 		assertEquals(2, list.size());
 		assertEquals(4, list.size());
 	}
-	
-	
+
 	@Test
 	public void testMockingInterfaceWithIndexValue() {
 		List list = mock(List.class);
-		
+
 		when(list.get(0)).thenReturn("Laxman");
-		
+
 		assertEquals("Laxman", list.get(0));
 		assertEquals(null, list.get(1));
 	}
-	
-	//Anyint return sme value if we get any number of index
-	
+
+	// Anyint return sme value if we get any number of index
+
 	@Test
 	public void testMockingInterfaceWithIndexValueAny() {
 		List list = mock(List.class);
-		
+
 		when(list.get(anyInt())).thenReturn("Laxman");
-		
+
 		assertEquals("Laxman", list.get(0));
 		assertEquals("Laxman", list.get(1));
 		assertEquals("Laxman", list.get(100));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testMockingInterfaceWithException() {
+		List list = mock(List.class);
+
+		when(list.get(anyInt())).thenThrow(new RuntimeException("Something"));
+		assertEquals(1, list.get(0));
 	}
 }
